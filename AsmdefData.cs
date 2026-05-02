87d1af008824252c2c8c60d971dfbf4adc1b2979
Atomiz <tytunipex@gmail.com>
2026-05-02 21:40:44 +0300

    Initial

---
+++ AsmdefData.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace DependencyManagement {
+
+    [Serializable]
+    public class AsmdefData {
+
+        public string name;
+
+        // General Options
+        public bool   allowUnsafeCode;
+        public bool   autoReferenced;
+        public bool   noEngineReferences;
+        public bool   overrideReferences;
+        public string rootNamespace;
+
+        /// ASMDEFs
+        public List<string> references;
+
+        /// DLLs
+        public List<string> precompiledReferences;
+
+        // Platforms
+        public List<string> includePlatforms;
+        public List<string> excludePlatforms;
+
+        public List<string>        defineConstraints;
+        public List<VersionDefine> versionDefines = new();
+
+        private string path;
+
+        public AsmdefData(string asmdefPath) {
+            JsonUtility.FromJsonOverwrite(File.ReadAllText(asmdefPath, Encoding.UTF8), this);
+            overrideReferences = true;
+            path               = asmdefPath;
+        }
+
+        public void WriteToFile(string asmdefPath = null) {
+            DistinctDefines();
+            File.WriteAllText(asmdefPath ?? path, JsonUtility.ToJson(this, true), Encoding.UTF8);
+        }
+
+        public void DistinctDefines() {
+            HashSet<string> defines = new();
+            versionDefines = versionDefines.Where(item => defines.Add(item.define)).ToList();
+        }
+
+        public void ClearReferencesAndDefines() {
+            versionDefines?.Clear();
+            references?.Clear();
+            precompiledReferences?.Clear();
+        }
+
+        [Serializable]
+        public class VersionDefine {
+
+            public string name;
+            public string expression;
+            public string define;
+
+            private VersionDefine(string name, string expression, string define) {
+                this.name       = name;
+                this.expression = expression;
+                this.define     = define;
+            }
+
+            public static VersionDefine Required(string package, string define = "") => new(package, string.Empty, define);
+            public static VersionDefine Located(string define)                       => new("Unity", string.Empty, define);
+            public static VersionDefine Missing(string define, string reference)     => new("Unity", $"Missing {reference}", define);
+
+            public override string ToString() => $"[ {define} | {name} | {expression} ]";
+
+        }
+
+    }
+
+}
---
+++ DependencyManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace DependencyManagement {
+
+    public abstract class DependencyManager : AssetPostprocessor {
+
+        protected static readonly List<AsmdefDependencies> AsmdefsDependencies = new();
+
+        [MenuItem("Tools/Atomiz/Dependency Manager/Reference Dependencies")]
+        public static void ReferenceDependencies() {
+            foreach (AsmdefDependencies dependencies in AsmdefsDependencies)
+                dependencies.ReferenceDependencies();
+
+            AssetDatabase.Refresh();
+        }
+
+        [MenuItem("Tools/Atomiz/Dependency Manager/Reset Soft Dependencies")]
+        private static void Reset() {
+            // Assets
+            foreach (string filePath in Directory.EnumerateFiles(Application.dataPath, "*.asmdef", SearchOption.AllDirectories)) {
+                Debug.Log(Path.GetFileName(filePath));
+
+                if (AsmdefsDependencies.Any(d => d.asmdef == Path.GetFileName(filePath)))
+                    reset(filePath);
+            }
+
+            Debug.Log(Path.Combine(Directory.GetParent(Application.dataPath)!.FullName, "Packages"));
+
+            // Packages
+            foreach (string filePath in Directory.EnumerateFiles(Path.Combine(Directory.GetParent(Application.dataPath)!.FullName, "Packages"), "*.asmdef", SearchOption.AllDirectories)) {
+                Debug.Log(Path.GetFileName(filePath));
+
+                if (AsmdefsDependencies.Any(d => d.asmdef == Path.GetFileName(filePath)))
+                    reset(filePath);
+            }
+
+            AssetDatabase.Refresh();
+            return;
+
+            void reset(string filePath) {
+                Debug.Log(filePath);
+                AsmdefData asmdef = new(filePath);
+                asmdef.ClearReferencesAndDefines();
+                asmdef.WriteToFile();
+            }
+        }
+
+        [MenuItem("Tools/Atomiz/Dependency Manager/Distinctify Soft Dependencies Version Defines")]
+        private static void Distinct() {
+            foreach (string path in AssetDatabase.FindAssets("t:AssemblyDefinitionAsset")
+                         .Select(AssetDatabase.GUIDToAssetPath)
+                         .Where(p => AsmdefsDependencies.Any(d => d.asmdef == Path.GetFileNameWithoutExtension(p)))) {
+                new AsmdefData(path).WriteToFile();
+            }
+
+            AssetDatabase.Refresh();
+        }
+
+    }
+
+}
